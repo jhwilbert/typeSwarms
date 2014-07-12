4e722e185f987571f5cb742ac2bb3d08272fccc4
André Bose do Amaral <inquiries@mcnto.com>
2014-07-12 23:21:17 +0400

    Cosmetics
    Cosmetics.

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,10 @@
 int main( ){
 
     ofAppGlutWindow window;
-	ofSetupOpenGL(&window, 1800,768, OF_WINDOW);			// <-------- setup the GL context
+	ofSetupOpenGL(&window, 1800,768, OF_FULLSCREEN);			// <-------- setup the GL context
 
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
 	// pass in width and height too:
 	ofRunApp( new testApp());
-
 }
